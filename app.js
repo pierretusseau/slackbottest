76bd400afc4f2adadf1bf5b1e4837a4814a2999d
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-11-15 00:14:31 +0100

    tentative correction 5

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.listen(port, function () {
 
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
-  var payload = {
+  var botPayload = {
   	text : 'Hello ' + userName + ', welcome to BTF Slack, where we kill young babies every sunday night.'
   };
 
