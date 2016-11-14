4d6eefb990239d58b6aa4bda8c148f984e6784c3
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-11-14 23:53:07 +0100

    tentative correction 1

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 1337;
 
-app.use(bodyParser.urlencoded({extended: true}));
+// bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {res.status(200).send('Hello World!'); });
+//main route
+app.get('/', function (req, res) { res.status(200).send('Hello World!'); });
 
-app.listen(port, function(){
-	console.log('Listening on port ' + port);
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
 });
 
-app.post('/hello', function(req, res, next){
-	var userName = req.body.user_name;
-	var botPayload = {
-		text: 'Hello ' + username + ', welcome to BTF Slack, where we kill young babies every sunday night.'
-	};
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+  	text : 'Hello ' + userName + ', welcome to BTF Slack, where we kill young babies every sunday night.'
+  };
 
-	if (userName !== 'slackbot') {
-		return res.status(200).json(botPayload);
-	} else {
-		return res.status(200).end();
-	}
+  if (userName !== 'slackbot') {
+  	return res.status(200).json(botPayload);
+  } else {
+  	return res.status(200).end();
+  }
 });
