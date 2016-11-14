216363c2d77c69a3c9312aab1c30ce40a0432203
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-11-15 00:21:04 +0100

    tentative correction loop 1

--- app.js
+++ app.js
@@ -15,12 +15,11 @@ app.listen(port, function () {
 });
 
 app.post('/hello', function (req, res, next) {
-  var userName = req.body.username;
   var botPayload = {
-  	text : 'Hello ' + userName + ', welcome to BTF Slack, where we kill young babies every sunday night.'
+  	text : 'Hello ' + username + ', welcome to BTF Slack, where we kill young babies every sunday night.'
   };
 
-  if (userName !== 'slackbot') {
+  if (userName !== 'Trouduc\'') {
   	return res.status(200).json(botPayload);
   } else {
   	return res.status(200).end();
