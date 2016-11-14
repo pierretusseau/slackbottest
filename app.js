81fa7dd79895b867df7eb077bfb9fdaecc8fde6c
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-11-14 23:39:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {res.status(200).send('Hello World!'); });
+
+app.listen(port, function(){
+	console.log('Listening on port ' + port);
+});
